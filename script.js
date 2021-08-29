daa8892033baa796aedc7bc96002c481ecfb7d66
Marta Skrzypczak <mart51w7@stud.kea.dk>
2021-08-29 18:49:32 +0200

    calculation works and adding to list

--- script.js
+++ script.js
@@ -7,19 +7,45 @@ function clickCalculate() {
   console.log(secondNumber);
   const dropDown = document.querySelector("#operator").value;
 
-  if (dropDown.value === "add") {
-    result = firstNumber + secondNumber;
-    console.log("does this shit work" + result);
-  }
-  //   } else if (dropDown.value === "sub") {
-  //     result = firstNumber - secondNumber;
-  //     console.log(result);
-  //   } else if (dropDown.value === "mul") {
-  //     result = firstNumber * secondNumber;
-  //     console.log(result);
-  //   } else dropDown.value === "div";
+  // const checkbox = document.querySelector("#decimals").value;
 
-  //   result = firstNumber / secondNumber;
+  if (dropDown === "add") {
+    result = Number(firstNumber + secondNumber);
+    console.log("does this shit work" + result);
+    const newNumber = document.querySelector("#firstnumber");
+    newNumber.value = result;
+    let list = document.createElement("li");
+    document.querySelector("#results").append(list);
+    list.textContent = result;
+  } else if (dropDown === "sub") {
+    result = firstNumber - secondNumber;
+    console.log(result);
+    const newNumber = document.querySelector("#firstnumber");
+    newNumber.value = result;
+  } else if (dropDown === "mul") {
+    result = firstNumber * secondNumber;
+    console.log(result);
+    const newNumber = document.querySelector("#firstnumber");
+    newNumber.value = result;
+  } else result = firstNumber / secondNumber;
+  console.log(result);
+  const newNumber = document.querySelector("#firstnumber");
+  newNumber.value = result;
 }
+//   } else if (dropDown.value === "sub") {
+//     result = firstNumber - secondNumber;
+//     console.log(result);
+//   } else if (dropDown.value === "mul") {
+//     result = firstNumber * secondNumber;
+//     console.log(result);
+//   } else dropDown.value === "div";
+
+//   result = firstNumber / secondNumber;
+
 document.querySelector("#clear").addEventListener("click", clearResults);
+
+// if (checkbox.checked) {
+//   if (checkbox.value === 1) {
+//   }
+// }
 function clearResults() {}
